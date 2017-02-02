0655a46a5d1e2c0f4924c6ee4eef9acf11483344
rachel <rachel@spirink.com>
2017-02-02 14:43:12 +0900

    su

--- tech_stack/src/components/LibraryList.js
+++ tech_stack/src/components/LibraryList.js
@@ -1,15 +1,33 @@
 import React, { Component } from 'react';
+import { ListView } from 'react-native';
 import { connect } from 'react-redux';
+import ListItem from './ListItem';
 
 class LibraryList extends Component {
+  componentWillMount() {
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2
+    });
+
+    this.dataSource = ds.cloneWithRows(this.props.libraries);
+  }
+
+  renderRow(library) {
+    return <ListItem library={library} />;
+  }
+
   render() {
-    console.log(this.props);
-    return;
+    return (
+      <ListView
+        dataSource={this.dataSource}
+        renderRow={this.renderRow}
+      />
+    );
   }
 }
 
 const mapStoreToProps = state => {
-  return { librariess: state.libraries };
+  return { libraries: state.libraries };
 };
 
 export default connect(mapStoreToProps)(LibraryList);
